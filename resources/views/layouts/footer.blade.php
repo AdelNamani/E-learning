dcc803d9d7658f53f37cf590746ac25ab6b5f8a8
Zellat Abdelkhalek <ha_zellat@esi.dz>
2019-04-30 14:34:25 +0100

    Add assets & Layouts

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,61 @@
+<footer>
+		<div class="container margin_120_95">
+			<div class="row">
+				<div class="col-lg-5 col-md-12 p-r-5">
+					<p><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></p>
+					<p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
+					<div class="follow_us">
+						<ul>
+							<li>Follow us</li>
+							<li><a href="#0"><i class="ti-facebook"></i></a></li>
+							<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+							<li><a href="#0"><i class="ti-google"></i></a></li>
+							<li><a href="#0"><i class="ti-pinterest"></i></a></li>
+							<li><a href="#0"><i class="ti-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 ml-lg-auto">
+					<h5>Useful links</h5>
+					<ul class="links">
+						<li><a href="#0">Admission</a></li>
+						<li><a href="#0">About</a></li>
+						<li><a href="#0">Login</a></li>
+						<li><a href="#0">Register</a></li>
+						<li><a href="#0">News &amp; Events</a></li>
+						<li><a href="#0">Contacts</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h5>Contact with Us</h5>
+					<ul class="contacts">
+						<li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
+						<li><a href="mailto:info@udema.com"><i class="ti-email"></i> info@udema.com</a></li>
+					</ul>
+					<div id="newsletter">
+					<h6>Newsletter</h6>
+					<div id="message-newsletter"></div>
+					<form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
+						<div class="form-group">
+							<input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+							<input type="submit" value="Submit" id="submit-newsletter">
+						</div>
+					</form>
+					</div>
+				</div>
+			</div>
+			<!--/row-->
+			<hr>
+			<div class="row">
+				<div class="col-md-8">
+					<ul id="additional_links">
+						<li><a href="#0">Terms and conditions</a></li>
+						<li><a href="#0">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<div id="copy">© 2017 Udema</div>
+				</div>
+			</div>
+		</div>
+	</footer>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,82 @@
+	<header class="header menu_2">
+		<div id="preloader"><div data-loader="circle-side"></div></div><!-- /Preload -->
+		<div id="logo">
+			<a href="index.html"><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></a>
+		</div>
+		<ul id="top_menu">
+			<li><a href="login.html" class="login">Login</a></li>
+			<li><a href="#0" class="search-overlay-menu-btn">Search</a></li>
+			<li class="hidden_tablet"><a href="admission.html" class="btn_1 rounded">Admission</a></li>
+		</ul>
+		<!-- /top_menu -->
+		<a href="#menu" class="btn_mobile">
+			<div class="hamburger hamburger--spin" id="hamburger">
+				<div class="hamburger-box">
+					<div class="hamburger-inner"></div>
+				</div>
+			</div>
+		</a>
+		<nav id="menu" class="main-menu">
+			<ul>
+				<li><span><a href="#0">Home</a></span>
+					<ul>
+						<li><a href="index.html">Home version 1</a></li>
+						<li><a href="index-2.html">Home version 2</a></li>
+						<li><a href="index-3.html">Home version 3</a></li>
+						<li><a href="index-4.html">Home version 4</a></li>
+						<li><a href="index-5.html">With Cookie bar (EU law)</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">Courses</a></span>
+					<ul>
+						<li><a href="courses-grid.html">Courses grid</a></li>
+						<li><a href="courses-grid-sidebar.html">Courses grid sidebar</a></li>
+						<li><a href="courses-list.html">Courses list</a></li>
+						<li><a href="courses-list-sidebar.html">Courses list sidebar</a></li>
+						<li><a href="course-detail.html">Course detail</a></li>
+                        <li><a href="course-detail-2.html">Course detail working form</a></li>
+						<li><a href="admission.html">Admission wizard</a></li>
+						<li><a href="teacher-detail.html">Teacher detail</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">Pages</a></span>
+					<ul>
+						<li><a href="#0">Menu 2</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="blog.html">Blog</a></li>
+						<li><a href="login.html">Login</a></li>
+						<li><a href="register.html">Register</a></li>
+						<li><a href="contacts.html">Contacts</a></li>
+						<li><a href="404.html">404 page</a></li>
+						<li><a href="agenda-calendar.html">Agenda Calendar</a></li>
+						<li><a href="faq.html">Faq</a></li>
+						<li><a href="help.html">Help</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">Extra Pages</a></span>
+					<ul>
+						<li><a href="media-gallery.html">Media gallery</a></li>
+						<li><a href="cart-1.html">Cart page 1</a></li>
+						<li><a href="cart-2.html">Cart page 2</a></li>
+						<li><a href="cart-3.html">Cart page 3</a></li>
+						<li><a href="pricing-tables.html">Responsive pricing tables</a></li>
+						<li><a href="coming_soon/index.html">Coming soon</a></li>
+						<li><a href="icon-pack-1.html">Icon pack 1</a></li>
+						<li><a href="icon-pack-2.html">Icon pack 2</a></li>
+						<li><a href="icon-pack-3.html">Icon pack 3</a></li>
+						<li><a href="icon-pack-4.html">Icon pack 4</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">Buy template</a></span></li>
+			</ul>
+		</nav>
+		<!-- Search Menu -->
+		<div class="search-overlay-menu">
+			<span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+			<form role="search" id="searchform" method="get">
+				<input value="" name="q" type="search" placeholder="Search..." />
+				<button type="submit"><i class="icon_search"></i>
+				</button>
+			</form>
+		</div><!-- End Search Menu -->
+	</header>
