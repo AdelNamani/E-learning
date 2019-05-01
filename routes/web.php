c87f6d9f1becf7f6ad6f500e48ac152e606e3331
AdelNamani <ga_namani@esi.dz>
2019-05-01 12:53:21 +0100

    Add mail verification

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ Route::get('/', 'CourseController@index')->name('home');
 
 Route::get('/profile','UserController@show')->name('profile')->middleware('auth');
 
-Route::get('/profile/edit','UserController@edit')->name('profile_edit')->middleware('auth');
+Route::get('/profile/edit','UserController@edit')->name('profile_edit')->middleware(['auth','verified']);
 
 Route::post('/profile/update_info','UserController@update_info')->name('profile_update_info')->middleware('auth');
 
 Route::post('/profile/update_password','UserController@update_password')->name('profile_update_password')->middleware('auth');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 //Route::get('/home', 'HomeController@index')->name('home');
