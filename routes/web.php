ace2435ffb4529fe1d74564e87f965a435e436f3
AdelNamani <ga_namani@esi.dz>
2019-05-04 12:36:44 +0100

    Add teacher and admin middlewares

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,29 @@ Auth::routes(['verify' => true]);
 
 Route::get('/', 'CourseController@index')->name('home');
 
-Route::get('/course/{id}' , 'CourseController@show')->name('course.show')->middleware('auth');
+Route::get('/course/{id}' , 'CourseController@show')->name('course.show')->middleware(['auth','verified']);
 
 /**----------------------------------------------------------------------------------------------------------------**/
 
-Route::get('/lesson/{id}','LessonController@show')->name('lesson.show')->middleware('auth');
+Route::get('/lesson/{id}','LessonController@show')->name('lesson.show')->middleware(['auth','verified']);
 
-Route::get('/lesson/{id}/complete','LessonController@complete')->name('lesson.complete')->middleware('auth');
+Route::get('/lesson/{id}/complete','LessonController@complete')->name('lesson.complete')->middleware(['auth','verified']);
 
 /**----------------------------------------------------------------------------------------------------------------**/
 
-Route::get('/chapter/{id}/quiz','ChapterController@quiz')->name('chapter.quiz')->middleware('auth');
+Route::get('/chapter/{id}/quiz','ChapterController@quiz')->name('chapter.quiz')->middleware(['auth','verified']);
 
-Route::post('/chapter/{id}/quiz','ChapterController@quizSubmit')->name('chapter.quizSubmit')->middleware('auth');
+Route::post('/chapter/{id}/quiz','ChapterController@quizSubmit')->name('chapter.quizSubmit')->middleware(['auth','verified']);
 
 /**----------------------------------------------------------------------------------------------------------------**/
 
 Route::get('/profile','UserController@show')->name('profile')->middleware('auth');
 
-Route::get('/profile/edit','UserController@edit')->name('profile.edit')->middleware(['auth']);
+Route::get('/profile/edit','UserController@edit')->name('profile.edit')->middleware(['auth','verified']);
 
-Route::post('/profile/update_info','UserController@update_info')->name('profile.update_info')->middleware('auth');
+Route::post('/profile/update_info','UserController@update_info')->name('profile.update_info')->middleware(['auth','verified']);
 
-Route::post('/profile/update_password','UserController@update_password')->name('profile.update_password')->middleware('auth');
+Route::post('/profile/update_password','UserController@update_password')->name('profile.update_password')->middleware(['auth','verified']);
 
 /**----------------------------------------------------------------------------------------------------------------**/
 
